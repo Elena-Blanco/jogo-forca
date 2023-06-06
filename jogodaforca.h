655996cf5665660a5bed65995e29eb548c84de8e
Elena-Blanco <128832230+Elena-Blanco@users.noreply.github.com>
2023-06-06 10:42:55 -0300

    Add files via upload

---
+++ jogodaforca.h
@@ -0,0 +1,10 @@
+#define TAMANHO_PALAVRA 20
+
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
